c57fe684f79878b257fc040cbb57819a50748a61
Stéphane Klein <stephaneklein221@gmail.com>
2019-03-31 03:07:22 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const mathjs = require("mathjs");
+
+(() => {
+  const input = document.querySelector("input[name=expression]");
+  const submit = document.querySelector("button[type=submit]");
+  const canvas1 = document.querySelector("#cartesian");
+  const canvas2 = document.querySelector("#polar");
+  const cartesianCtx = canvas1.getContext("2d");
+  const polarCtx = canvas2.getContext("2d");
+  const unitsPerAxe = 16;
+
+  submit.addEventListener("click", event => {
+    event.preventDefault();
+    draw(input.value);
+  });
+
+  draw(input.value);
+
+  function draw(expression) {
+    const compiled = mathjs.compile(expression);
+    drawBasis(cartesianCtx);
+    drawBasis(polarCtx);
+    drawCartesian(cartesianCtx, compiled, unitsPerAxe);
+    drawPolar(polarCtx, compiled, unitsPerAxe);
+  }
+})();
+
+function drawBasis(ctx) {
+  const width = ctx.canvas.clientWidth;
+  const height = ctx.canvas.clientHeight;
+
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "rgb(100, 100, 100)";
+
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2);
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(width / 2, 0);
+  ctx.lineTo(width / 2, height);
+  ctx.stroke();
+}
+
+function drawCartesian(ctx, expression, unitsPerAxe) {
+  const halfAxe = unitsPerAxe / 2;
+  const xInc = unitsPerAxe / 10000;
+  const points = [];
+
+  for (let x = -halfAxe; x < halfAxe; x += xInc ) {
+    const y = expression.eval({ x });
+    points.push([x, y]);
+  }
+
+  drawPoints(ctx, points, unitsPerAxe);
+}
+
+function drawPolar(ctx, expression, unitsPerAxe) {
+  const angleInc = Math.PI / 1000;
+  const points = [];
+
+  for (let t = 0; t < 2 * Math.PI; t += angleInc) {
+    const r = expression.eval({ x: t });
+    const x = r * Math.cos(t);
+    const y = r * Math.sin(t);
+    points.push([x, y]);
+  }
+
+   drawPoints(ctx, points, unitsPerAxe);
+}
+
+function drawPoints(ctx, points, unitsPerAxe) {
+  const width = ctx.canvas.clientWidth;
+  const height = ctx.canvas.clientHeight;
+  const scale = width / unitsPerAxe;
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "rgb(0, 50, 100)";
+  ctx.beginPath();
+
+  for (let i = 0; i < points.length; ++i) {
+    const [x, y] = points[i];
+
+    if (y === Infinity || y === -Infinity) {
+      ctx.stroke();
+      ctx.beginPath();
+      continue;
+    }
+
+    const mappedX = width / 2 - x * scale;
+    const mappedY = height / 2 - y * scale;
+
+    ctx.lineTo(mappedX, mappedY);
+  }
+
+  ctx.stroke();
+}
